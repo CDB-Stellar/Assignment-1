819b82ddc4beb7a693d2b4d22934bc78c108950d
Chloe <chloe_baker@live.com>
2020-10-08 17:14:46 -0400

    Form Function
    - put the function for form information collection in app.js

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -8,3 +8,18 @@
     }
     window.addEventListener("load", Start); //when the window finishes loading it will call the Start function
 })();
+
+//Called when the form submit button is clicked
+function getFormInfo() {
+    //Get the form information
+    let formInfo = document.getElementById("contactForm");
+    
+    //Display it to make sure it worked
+    let text = "";
+    let i;
+    for (i = 0; i < formInfo.length; i++) 
+    {
+      text += formInfo.elements[i].value + "\n";
+    }
+    window.alert(text);
+}
